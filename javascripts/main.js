561f8d9a54d4eb7afb58a4d1fbf269cdd3ac0bc7
AHN JAE-HA <eu81273@gmail.com>
2015-03-17 00:25:19 +0900

    first commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,87 @@
-console.log('This would be the main JS file.');
+'use strict';
+
+var app = angular.module('angularApplication', ['angular.panels']);
+
+//default controller
+app.controller('defaultController', ['$scope', 'panels', function ($scope, panels) {
+
+	$scope.leftOpen = function () {
+		$scope.$broadcast('leftHello', {message : $scope.message}); 
+	};
+
+	$scope.rightOpen = function () {
+		$scope.$broadcast('rightHello', {message : $scope.message}); 
+	};
+
+	$scope.topOpen = function () {
+		$scope.$broadcast('topHello', {message : $scope.message}); 
+	};
+
+	$scope.bottomOpen = function () {
+		$scope.$broadcast('bottomHello', {message : $scope.message}); 
+	};
+
+}]);
+
+//left panel controller
+app.controller('leftCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+	$scope.$on('leftHello', function(event, args) {
+
+		$scope.message = args.message;
+
+		panels.open("left");
+	});
+
+	$scope.closePane = function () {
+		panels.close();
+	};
+}]);
+
+
+//right panel controller
+app.controller('rightCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+	$scope.$on('rightHello', function(event, args) {
+
+		$scope.message = args.message;
+
+		panels.open("right");
+	});
+
+	$scope.closePane = function () {
+		panels.close();
+	};
+}]);
+
+
+//top panel controller
+app.controller('topCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+	$scope.$on('helloTop', function(event, args) {
+
+		$scope.message = args.message;
+
+		panels.open("top");
+	});
+
+	$scope.closePane = function () {
+		panels.close();
+	};
+}]);
+
+
+//bottom panel controller
+app.controller('bottomCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+	$scope.$on('bottomHello', function(event, args) {
+
+		$scope.message = args.message;
+
+		panels.open("bottom");
+	});
+
+	$scope.closePane = function () {
+		panels.close();
+	};
+}]);
